d74ba41255722de254126cfeed3092b8f65f598e
lucas-borges-belo <lucaspdfborges@gmail.com>
2022-04-15 14:29:14 -0300

    added integration with AI

--- post.py
+++ post.py
@@ -2,8 +2,6 @@ import requests
 import sqlite3
 import json
 
-
-
 """
  CREATE TABLE dados( 
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,11 +16,9 @@ import json
 INSERT INTO dados (SENSOR_ID,DADOTIPO_ID,VALOR,TIMESTAMP) VALUES (4,1,1100,"2019-08-24T16:05:22Z");
 """
 
-
 secret_dict = json.load(open('secret.json', 'r'))
 post_data_url = secret_dict['post_data_url']
-
-
+post_state_url = secret_dict['post_state_url']
 
 def data_to_post_later(sensor_id, dadotipo_id, value, timestamp):
 
@@ -35,7 +31,16 @@ def data_to_post_later(sensor_id, dadotipo_id, value, timestamp):
 
     return query
 
+def state_to_post_later(state_id, timestamp):
 
+    conn = sqlite3.connect('rpi.db')
+    c = conn.cursor()
+    query = 'INSERT INTO dados (STATE_ID,TIMESTAMP) VALUES (%d,"%s");'%(state_id, timestamp)
+    c.execute(query)
+    conn.commit()
+    c.close()
+
+    return query
 
 def post_late_data(post_data_url, access_token):
 
@@ -67,8 +72,62 @@ def post_late_data(post_data_url, access_token):
     return r
 
 
+def post_late_state(post_state_url, access_token):
 
-# Post when Online 
+    # Open connection and read data
+    conn = sqlite3.connect('rpi.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM estados ORDER BY ID ASC LIMIT 1')
+    fetched_data = c.fetchall()[0]
+    
+    data = {
+        "estado": fetched_data[1],
+        "timestamp": fetched_data[2],
+    }
+
+    print(" # Late DB: ",data)
+
+    # Delete data and close connection
+    c.execute('DELETE FROM estados WHERE id = '+str(fetched_data[0]))
+    conn.commit()
+    c.close()
+
+    # Post data to API / Database
+    bearer_auth = 'Bearer '+ str(access_token)
+    headers = { 'Authorization': bearer_auth }
+    r = requests.post(post_state_url, data=data, headers=headers)
+
+    return r
+
+# Check if arrays are complete to post state
+
+def can_post_state(state_input):
+    for key in state_input:
+        if len(state_input[key])<100:
+            return False
+    return True
+
+def input_state_trim(state_input):
+    for key in state_input:
+        state_input[key] = state_input[key][len(state_input[key])-100:len(state_input[key])]
+    return state_input 
+
+# post state when online
+def post_state(state_id, timestamp, access_token):
+
+    data = {
+        "estado": state_id,
+        "timestamp": timestamp,
+    }
+
+    bearer_auth = 'Bearer '+ str(access_token)
+    headers = { 'Authorization': bearer_auth }
+
+    r = requests.post(post_state_url, data=data, headers=headers)
+
+    return r
+
+# Post data when Online 
 def post_data(sensor_id, dadotipo_id, value, timestamp, access_token):
 
     data = {
--- socket_udp.py
+++ socket_udp.py
@@ -2,20 +2,43 @@ import datetime
 import socket as s
 import ast
 import json
+import os
 import numpy as np
+from IA.Ainoa import Barragem
 
-from post import post_data, data_to_post_later, post_late_data
+
+from post import post_data, post_state, data_to_post_later, post_late_data, post_late_state, can_post_state, input_state_trim, state_to_post_later
 from auth import authenticate, refresh_authentication
 
 # ----------------------------------
-# JSON VARIABLES --------------------
+# A.I. VARIABLES -------------------
+# ----------------------------------
+
+barragem_path = os.path.dirname(os.path.realpath(__file__)) + '/AI'
+Paranoa = Barragem(barragem_path)
+
+# ----------------------------------
+# JSON VARIABLES -------------------
 # ----------------------------------
 
 dadotipo_dict = json.load(open('dadostipos.json', 'r'))
 sensores_dict = json.load(open('sensores.json', 'r'))
 
+
 secret_dict = json.load(open('secret.json', 'r'))
 post_data_url = secret_dict['post_data_url']
+post_state_url = secret_dict['post_state_url']
+state_input_blank = {
+    'R8CEBEHE':np.array([]), 'R8CEBEHN':np.array([]), 
+    'R8CEBEHZ':np.array([]), 'R016AEHE':np.array([]), 
+    'R016AEHN':np.array([]), 'R016AEHZ':np.array([]), 
+    'RBAE5EHZ':np.array([]), 'RBAE5ENE':np.array([]), 
+    'RBAE5ENN':np.array([]), 'RBAE5ENZ':np.array([]), 
+    'RE647EHE':np.array([]), 'RE647EHN':np.array([]), 
+    'RE647EHZ':np.array([]), 'RFB89EHE':np.array([]), 
+    'RFB89EHZ':np.array([]), 'RFB89EHN':np.array([])
+    }
+state_input =  state_input_blank.copy()
 
 # ----------------------------------
 # AUTH VARIABLES --------------------
@@ -43,51 +66,67 @@ def connect_and_post():
         sock.bind((host, port))
 
         while True:
-                data, addr = sock.recvfrom(1024)   
-                dict_str = data.decode("UTF-8")    
+            data, addr = sock.recvfrom(1024)   
+            dict_str = data.decode("UTF-8")    
 
-                dadotipo_str = dict_str[2:5]
-                dadotipo_id = dadotipo_dict[dadotipo_str]
-                
-                unixtime_str = dict_str[8:22]
-                timestamp =  datetime.datetime.utcfromtimestamp(float(unixtime_str)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-                
-                dados_str = "{"+dict_str[24:len(dict_str)]
-                dados = ast.literal_eval(dados_str)
+            dadotipo_str = dict_str[2:5]
+            dadotipo_id = dadotipo_dict[dadotipo_str]
+            
+            unixtime_str = dict_str[8:22]
+            timestamp =  datetime.datetime.utcfromtimestamp(float(unixtime_str)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            
+            dados_str = "{"+dict_str[24:len(dict_str)]
+            dados = ast.literal_eval(dados_str)
 
-                for dado in dados:
+
+            for dado in dados:
+                try:
+                    # Post Current UDP data
+                    post_request = post_data(sensor_id, dadotipo_id, dado, timestamp, access_token)
+                    print(post_request.status_code, post_request.json())
+
+                    # States input for the AI
+                    state_key = sensor_name + dadotipo_str
+                    state_input[state_key] =  np.append(state_input[state_key], dado)
+
+                    if can_post_state(state_input):
+                        state_input = input_state_trim(state_input)
+                        Paranoa.atualiza(state_input)
+                        state_id = Paranoa.estado + 1 # Database starts w/ 1 not 0, thats why the +1
+                        post_state(state_id, timestamp, access_token)
+                        state_input =  state_input_blank.copy()
+
+                    # Post last local DB data
                     try:
-                        # Post Current UDP data
-                        post_request = post_data(sensor_id, dadotipo_id, dado, timestamp, access_token)
-                        print(post_request.status_code, post_request.json())
-
-                        # Post last local DB data
-                        try:
-                            post_late_data(post_data_url, access_token)
-                        except:
-                            print('db synced')
-
-
-                        if post_request.status_code==401:
-                            access_token, refresh_token  = authenticate('raspberry','rpshake2021')
-                    
-                        if post_request.status_code!=201:
-                            raise Exception('Auth without credentials')
-
+                        post_late_data(post_data_url, access_token)
+                        post_late_state(post_state_url, access_token)
                     except:
-                        entry = data_to_post_later(sensor_id, dadotipo_id, dado, timestamp)
-                        print(entry)
+                        print('db synced')
 
-                    
+                    if post_request.status_code==401:
+                        access_token, refresh_token  = authenticate('raspberry','rpshake2021')
                 
+                    if post_request.status_code!=201:
+                        raise Exception('Auth without credentials')
+
+                except:
+                    entry = data_to_post_later(sensor_id, dadotipo_id, dado, timestamp)
+                    if can_post_state(state_input):
+                        state_input = input_state_trim(state_input)
+                        Paranoa.atualiza(state_input)
+                        state_id = Paranoa.estado + 1 # Database starts w/ 1 not 0, thats why the +1
+                        entry_state = state_to_post_later(sensor_id, dadotipo_id, dado, timestamp)
+                        state_input =  state_input_blank.copy()
+
+                    print(entry)
+                    print(entry_state)
+
 # ----------------------------------
 # UDP SOCKET  ----------------------
 # ----------------------------------
 
 def socket_udp():
-
     global access_token, refresh_token
-    
     while True:
         connect_and_post()
 
